dae965a790fac21fc84b588d364af4ecf7a1464d
Tuhin Chandra <tuhinchandra2k04@gmail.com>
2024-06-15 11:44:28 +0530

    chore: Add styles and scripts for homepage layout and interactivity

---
+++ script.js
@@ -0,0 +1,34 @@
+var crsr = document.querySelector("#cursor");
+var crsrBlur = document.querySelector("#cursor-blur");
+document.addEventListener("mousemove",function(dets){
+    crsr.style.left = dets.x -10 + "px";
+    crsr.style.top = dets.y -10 + "px";
+})
+document.addEventListener("mousemove",function(dets){
+    crsrBlur.style.left = dets.x - 250 + "px";
+    crsrBlur.style.top = dets.y - 250 + "px";
+})
+gsap.to("#nav",{
+    backgroundColor : "#000",
+    duration : 0.5,
+    height : "110px",
+    scrollTrigger : {
+        trigger : "#nav",
+        scroller : "body",
+        // markers : true,
+        start : "top -10%",
+        end : "top -11%",
+        scrub : 1,
+    }
+})
+gsap.to("#main",{
+    backgroundColor : "#000",
+    scrollTrigger : {
+        trigger : "#main",
+        scroller : "body",
+        markers : true,
+        start : "top -25%",
+        end : "top -70%",
+        scrub : 2,
+    }
+})
